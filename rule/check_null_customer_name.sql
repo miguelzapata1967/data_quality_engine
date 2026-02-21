43e6e44020cbb9ff3ce23a1dee1e5e84a6eda635
miguelzapata1967 <miguelzapata1967@gmail.com>
2026-02-20 20:02:20 -0800

    Add percentage calculation for null customer names

--- rule/check_null_customer_name.sql
+++ rule/check_null_customer_name.sql
@@ -1,7 +1,8 @@
 -- Rule: Check for NULL customer_name values 
 -- Purpose: Ensure customer_name is always populated in the customers table.
 SELECT
-      COUNT(*) AS null_customer_name_count
+      COUNT(*) AS null_customer_name_count,
+      COUNT(*)*1.0 / (SELECT COUNT(*) from customers) AS null_customer_name_persentage
 FROM
       customers
 WHERE customer_name IS NULL;
